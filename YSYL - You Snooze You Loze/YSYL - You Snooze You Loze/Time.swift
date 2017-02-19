6b3774f71076caf340d6a235a33f1a1cad9e3324
Kelden Lin <keldenlin@gmail.com>
2017-02-18 22:36:05 -0800

    Polished up relationship between time and alarm class with the view modules.

--- YSYL - You Snooze You Loze/YSYL - You Snooze You Loze/Time.swift
+++ YSYL - You Snooze You Loze/YSYL - You Snooze You Loze/Time.swift
@@ -10,6 +10,7 @@ import Foundation
 
 class Time {
     var time:Int
+    var isPm = false
 
     init(time:Int){
         self.time = time
@@ -26,9 +27,11 @@ class Time {
     func getHours() -> Int{
         let hours = time / 60
         if hours > 12{
+            isPm = true
             return hours - 12
         }
         else{
+            isPm = false
             return hours
         }
     }
@@ -37,7 +40,15 @@ class Time {
         return time % 60
     }
     
+    func getIsPm() -> Bool{
+        return isPm
+    }
+    
     func getTimeInMinutes(hours:Int, minutes:Int) -> Int{
         return (hours * 60) + minutes
+    }
+    
+    func getTimeInHoursMinutes(minutes:Int) -> (hours:Int, minutes:Int) {
+        return (self.getHours(), self.getMinutes())
     }
 }
