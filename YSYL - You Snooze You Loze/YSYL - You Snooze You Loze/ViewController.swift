5745fc1af9680d9193a551ce8d49ec2263fae287
Kelden Lin <keldenlin@gmail.com>
2017-02-18 16:17:32 -0800

    Initial push. Template storyboard.

---
+++ YSYL - You Snooze You Loze/YSYL - You Snooze You Loze/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  YSYL - You Snooze You Loze
+//
+//  Created by Kelden Lin on 2/18/17.
+//  Copyright © 2017 Revolution 9. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
